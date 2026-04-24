7f1b81407ee39d9b9ec023e42975effd40818d9d
Minnelli Mercedes <minnellirodriguezmercedes0826@gmail.com>
2026-04-24 01:30:18 -0400

    Agregar archivos de proyecto.

---
+++ ShippingCalculator/Controllers/EnvioController.cs
@@ -0,0 +1,42 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using ShippingCalculator.Models;
+
+namespace ShippingCalculator.Controllers
+{
+    public class EnvioController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calcular(Envio modelo)
+        {
+            if (modelo.Peso <= 0)
+            {
+                ViewBag.Error = "Ingrese un peso válido";
+                return View("Index");
+            }
+
+            switch (modelo.Pais)
+            {
+                case "India":
+                    modelo.Costo = modelo.Peso * 5;
+                    break;
+
+                case "US":
+                    modelo.Costo = modelo.Peso * 8;
+                    break;
+
+                case "UK":
+                    modelo.Costo = modelo.Peso * 10;
+                    break;
+            }
+
+            return View("Index", modelo);
+        }
+    }
+}
---
+++ ShippingCalculator/Models/Envio.cs
@@ -0,0 +1,9 @@
+﻿namespace ShippingCalculator.Models
+{
+    public class Envio
+    {
+            public double Peso { get; set; }
+            public string Pais { get; set; }
+            public double Costo { get; set; }
+    }
+}
